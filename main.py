ed06a72b74f113f583b0f27c1b2b127b2303e83b
foxyoung <foxyoung24@qq.com>
2017-08-06 02:48:41 +0800

    btc data

--- main.py
+++ main.py
@@ -13,14 +13,30 @@
 # limitations under the License.
 
 import webapp2
+import requests
+import re
+import numpy as np
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('testing!')
+        page = requests.get('http://api.chbtc.com/data/v1/kline?currency=eth_cny&type=30min&size=90')
+        page.encoding = page.apparent_encoding
+        eos_close =  re.findall(r'(\d+.\d+),\d+.\d+][,|\]]',page.text)
+        #print(eos_close)
+        self.response.write(calAve(eos_close))
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
+
+
+def calAve(data):
+    data = [float(i) for i in data]
+    for i in range(0,len(data)-29):
+        ma5 = np.average(data[25+i:30+i])
+        ma10 = np.average(data[20+i:30+i])
+        ma30 = np.average(data[i:30+i])
+        print(i , ma5 , ma10  ,  ma30)
